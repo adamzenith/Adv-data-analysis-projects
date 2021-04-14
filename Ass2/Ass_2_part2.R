98b0e1bf46d95591a5136342c9d9ed03c6b3c486
adamzenith <49098682+adamzenith@users.noreply.github.com>
2021-04-14 12:32:41 +0200

    new

--- Ass2/Ass_2_part2.R
+++ Ass2/Ass_2_part2.R
@@ -2,8 +2,13 @@
 # Load data 
 # _________________
 
+library(gclus)
+library ( corrplot )
+library(car)
+library(rcompanion)
+
 clothing <- read.table(file = '/Users/idabukhvillesen/Dropbox/8 SEMESTER/Adv. Data Ana. & Stat. Mod./Assignments/Assignment 2/dat_count.csv', sep = ";")
-clothing <- read.table(file = 'C:/Users/Bruger/Documents/GitHub/Adv-data-analysis-projects/Ass2/dat_count.csv', sep = ";")
+clothing <- read.table(file = 'C:/Users/matth/Documents/GitHub/Adv-data-analysis-projects/Ass2/dat_count.csv', sep = ";")
 # prepare clo data
 clothing$clo.adj <- clothing$clo
 clothing$clo.adj[clothing$clo.adj > 0] = 1
@@ -50,3 +55,7 @@ Anova ( model0.glm, type = 'II' )
 par ( mfrow =c(2 ,2))
 plot(model0.glm)
 
+
+par ( mfrow =c(2 ,1))
+interaction.plot(clothing$clo, clothing$sex, clothing$tInOp, xlab = 'Times Changed', ylab = 'Temperature Indoor', main= '')
+interaction.plot(clothing$clo, clothing$sex, clothing$tOut, xlab = 'Times Changed', ylab = 'Temperature Outdoor', main= '')
--- Ass2/Ass_2_part3.R
+++ Ass2/Ass_2_part3.R
@@ -2,30 +2,32 @@
 # Part 3 
 
 library(ordinal)
+library(rcompanion)
+library(car)
 
 ### LOAD DATA 
-fan.data <- read.table(file = '/Users/idabukhvillesen/Dropbox/8 SEMESTER/Adv. Data Ana. & Stat. Mod./Assignments/Assignment 2/CeilingFan.csv', sep = ";")
+fan.data <- read.table(file ='C:/Users/matth/Documents/GitHub/Adv-data-analysis-projects/Ass2/CeilingFan.csv', sep = ";")
 
 fan.data$fanSpeed.f <- factor(fan.data$fanSpeed)
 fan.data$fanType.f <- factor(fan.data$fanType)
 fan.data$TSV <- factor(fan.data$TSV)
 
 # TEST FOR INDEPENDENCE 
-table(fan.data$TSV,fan.data$fanSpeed) #contingency 
-chisq.test(fan.data$TSV,fan.data$fanSpeed) #low p-value -> independent
+coTable = table(fan.data$TSV,fan.data$fanSpeed) #contingency 
+chisq.test(table(fan.data$TSV,fan.data$fanSpeed)) #low p-value -> independent
 fisher.test(fan.data$TSV,fan.data$fanSpeed)
-
-# MAKE MODELS WITH ORDINAL PACKAGE 
+cramerV(coTable,bias.correct =TRUE,conf = 0.95,ci=T)
+0.# MAKE MODELS WITH ORDINAL PACKAGE 
 # ordinal variables (ranked data, distance between values not known)
 model0.clm <- clm( TSV ~ fanSpeed, data = fan.data )
 model0.clm 
 model0f.clm <- clm( TSV ~ fanSpeed.f, data = fan.data )
 model0f.clm
-anova(model0.clm) 
+Anova(model0.clm,type = 'II') 
 anova(model0f.clm)
 anova(model0f.clm,model0.clm) #independence not assumed 
-
-
+par(mfrow=c(2,2))
+plot(model0.clm)
 # CLM FOR TWO DEPENDENT VARIABLES 
 model1f.clm <- clm( TSV ~ fanSpeed.f + fanType.f, data = fan.data )
 model1f.clm
