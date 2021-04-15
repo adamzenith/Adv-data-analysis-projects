0ab8b3d1c6cd6d363e119bc8aba5fcd54b4e42db
idabv <43176866+idabv@users.noreply.github.com>
2021-04-15 15:48:42 +0200

    Part A final code

--- Ass2/Ass_2_ida.R
+++ Ass2/Ass_2_ida.R
@@ -7,7 +7,7 @@ setwd('/Users/idabukhvillesen/Dropbox/8 SEMESTER/Adv. Data Ana. & Stat. Mod./Ass
 library(gclus)
 library ( corrplot )
 library(car)
-library(rcompanion)
+
 
 
 ## Data 
@@ -19,16 +19,16 @@ summary(ozone)
 plot(ozone)
 
 ## Correlations
+par(mfrow=c(1,2))
 corrplot (cor( ozone ))
 
 # DENSITY PLOT OF OZONE CONCENTRATION
 Ozone = ozone$Ozone
-par(mfrow=c(1,1))
 # Density plot of Clo:
 f<-function(x){dnorm(x,mean=mean(Ozone,na.rm=T),
                      sd=sd(ozone$Ozone,na.rm=T))}
 hist(Ozone,xlab='Ozone',probability=T)
-curve(f,0,40,lwd=3,col="red",add=T)
+curve(f,0,40,lwd=3,col="blue",add=T)
 rm(f)
 par(mfrow=c(1,1))
 
@@ -76,14 +76,14 @@ plot ( model4 )
 model5 <- lm( Ozone ~ Temp+InvHt+Hum , data = ozone )
 summary ( model5 )
 Anova ( model5, type = 'II' )
-par ( mfrow =c(2 ,2))
+par ( mfrow =c(1 ,2))
 plot ( model5 )
 # NOW ALL SIGNIFICANT 
 
 model6 <- lm( log(Ozone) ~ Temp+InvHt+Hum , data = ozone )
 summary ( model6 )
 Anova ( model6, type = 'II' )
-par ( mfrow =c(2 ,2))
+par ( mfrow =c(1 ,2))
 plot ( model6 )
 # With log transform the residuals become way better 
 # Also the leverage plot is better 
@@ -91,7 +91,7 @@ plot ( model6 )
 # Probably as a distribution assumption and link function
 # ie. --> make it into a generalized model!! 
 
-
+par ( mfrow =c(1 ,2))
 ## Turning it into a GENERALIZED MODEL
 model00.glm<- glm( Ozone ~ Temp+InvHt+Pres+Vis+Hgt+Hum+InvTmp+Wind , family = poisson, data = ozone )
 summary ( model00.glm)
@@ -120,7 +120,7 @@ plot(model03.glm)
 
 model04.glm<- glm( Ozone ~ Temp+InvHt+Vis+Hum , family = poisson, data = ozone )
 summary ( model04.glm)
-Anova ( model04.glm, type = 'II' ) #remove invtmp
+Anova ( model04.glm, type = 'II' ) 
 par ( mfrow =c(2 ,2))
 plot(model04.glm)
 # FINAL PARAMETERS
@@ -141,10 +141,10 @@ plot(model1.glm)
 # Not a big difference
 
 model12.glm<- glm( Ozone ~ Temp+InvHt+Hum+Vis , family = Gamma(link="log"), data = ozone ) 
-summary ( model2.glm)
-Anova ( model2.glm, type = 'II' )
+summary ( model12.glm)
+Anova ( model12.glm, type = 'II' ) #remove Vis 
 par ( mfrow =c(2 ,2))
-plot(model2.glm)
+plot(model12.glm)
 
 
 model2.glm<- glm( Ozone ~ Temp+InvHt+Hum , family = Gamma(link="log"), data = ozone ) 
@@ -154,10 +154,10 @@ par ( mfrow =c(2 ,2))
 plot(model2.glm)
 
 model13.glm<- glm( Ozone ~ Temp+InvHt+Hum+Vis , family = Gamma(link="inverse"), data = ozone ) 
-summary ( model3.glm)
-Anova ( model3.glm, type = 'II' )
+summary ( model13.glm)
+Anova ( model13.glm, type = 'II' )
 par ( mfrow =c(2 ,2))
-plot(model3.glm)
+plot(model13.glm)
 
 
 model3.glm<- glm( Ozone ~ Temp+InvHt+Hum , family = Gamma(link="inverse"), data = ozone ) 
@@ -186,21 +186,62 @@ logLik(model3.glm)
 ## Compare lm (model6) and glm model (model0.glm, poisson)
 
 # make lm as glm for comparison
-model6.1 <- glm( log(Ozone) ~ Temp+InvHt+Hum , family = gaussian(link = "identity"),data = ozone )
-summary ( model6.1 )
-Anova ( model6.1, type = 'II' )
-par ( mfrow =c(2 ,2))
-plot ( model6.1 )
-library(MASS)
-# quantative test 
-logLik(model0.glm)
-logLik(model6.1)
-AIC(model0.glm, model6)
-par(mfrow=c(1,1))
-boxcox(model5)
-## IS THE GAUSSIAN GLM / LM REALLY BETTER??? 
+#model6.1 <- glm( log(Ozone) ~ Temp+InvHt+Hum , family = gaussian(link = "identity"),data = ozone )
+#summary ( model6.1 )
+#Anova ( model6.1, type = 'II' )
+#par ( mfrow =c(2 ,2))
+#plot ( model6.1 )
 
-par(mfrow=c(2,2))
-plot(model6)
-plot(model0.glm)
+# quantative test 
+#logLik(model0.glm)
+#logLik(model6.1)
+#AIC(model0.glm, model6)
+
+# BASE THE CONCLUSION ON THE VISUAL RESIDUAL PLOTS 
+par ( mfrow =c(1,1))
+plot(exp(model6$fitted.values), model6$residuals,xlab = "Fitted values", ylab = "Residuals", main = "Residual comparison",ylim=c(-1.5,2.0),xlim=c(0,30),cex=0.7, pch=10)
+points(model0.glm$fitted.values, model0.glm$residuals, pch=16, col=2,cex=0.7)
+legend("topright", legend = c("lm","glm"),col=c(1,2), pch=c(10,16))
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted")
+
+
+## Presentation of model 
+## PREDICTIONS 
+
+# make 80/20 training test and make predictions on the data 
+dt = sort(sample(nrow(ozone), nrow(ozone)*.8))
+train <- ozone[dt,]
+test <- ozone[-dt,]
+
+# Calculate model on training data 
+model.f.glm<- glm( Ozone ~ Temp+InvHt+Hum+Vis , family = poisson, data = train )
+summary ( model.f.glm)
+Anova ( model.f.glm, type = 'II' )
+par ( mfrow =c(2 ,2))
+plot(model.f.glm)
+
+# predict from test data 
+pred.f <- predict(model.f.glm, newdata = test,
+        type = c("response"),
+        se.fit = FALSE, dispersion = NULL, terms = NULL,
+        na.action = na.pass)
+
+# predict from original ozone data set 
+pred.ref <- predict(model0.glm, newdata = ozone,
+                  type = c("response"),
+                  se.fit = FALSE, dispersion = NULL, terms = NULL,
+                  na.action = na.pass)
+
+
+# calculate RMSE for the two predictions 
+sqrt(mean((pred.f-test$Ozone)^2))
+sqrt(mean((pred.ref-ozone$Ozone)^2))
+
+#plot predictions 
+par ( mfrow =c(1 ,1))
+plot(ozone$Ozone, pred.ref,main="Final Model Predictions",xlab = "True values",ylab = "Predicted values")
+lines(c(0:35),c(0:35),lty=3)
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted")
+points(test$Ozone, pred.f,pch=16, col=2,cex=1.15)
+legend("bottomright", legend = c("Original full data-set predictions","Test-set predictions"),col=c(1,2), pch=c(1,16),cex=0.85)
 
