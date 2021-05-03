73a563254ec94d81adf7e008c4988aaf8fbd10a3
helena8hansen <57371016+helena8hansen@users.noreply.github.com>
2021-05-03 16:44:43 +0200

    Create Adv-Ass3-Helena.R

---
+++ Ass3/Adv-Ass3-Helena.R
@@ -0,0 +1,90 @@
+# Assignment 3 - Adv. Data Analysis and Statistical Modelling
+library(lme4)
+library(readr)
+library(xtable)
+library(plyr)
+library(corrplot)
+library(nlme)
+library(gclus)
+library(stats)
+library(emmeans)
+
+setwd("~/Documents/Studie/Mst-Sem2-2021/Advanced Data Analysis and Stastitical Modelling/Assignment 3")
+
+Clothing = read.csv('clothingFullAss03.csv')
+
+## Exploratory Analysis #### 
+
+# The subject is measured 6 times a day for 3 days.
+
+par(mfrow=c(1,2))
+with(Clothing, {
+  interaction.plot(subjId, day, clo, legend=FALSE, 
+                   bty="n", col=2:11, xtick = TRUE)
+  interaction.plot(sex, day, clo, legend=FALSE, 
+                   bty="n", col=2:11, xtick = TRUE)})
+par(mfrow=c(1,1))
+
+
+par(mfrow=c(1,3))
+boxplot(Clothing$clo ~Clothing$sex, col=2:6, xlab = "Sex", ylab = "Clothing Level")
+boxplot(Clothing$clo ~Clothing$day, col=2:6, xlab = "Day", ylab = "Clothing Level")
+boxplot(Clothing$clo ~Clothing$subjId, col=2:6, xlab = "SubjId", ylab = "Clothing Level")
+
+par(mfrow=c(1,2))
+boxplot(Clothing$tInOp ~Clothing$day, col=2:6, xlab = "Day", ylab = "Temp. Indoor")
+boxplot(Clothing$tOut ~Clothing$day, col=2:6, xlab = "Day", ylab = "Temp. Outdoor")
+
+par(mfrow=c(1,1))
+# DENSITY PLOT OF CLOTHING  
+clothing = Clothing$clo
+# Density plot of Clo: 
+f<-function(x){dnorm(x,mean=mean(clothing,na.rm=T),
+                     sd=sd(clothing,na.rm=T))} 
+hist(clothing,xlab='Clothing',probability=T)
+curve(f,0,1,lwd=3,col="red",add=T) 
+rm(f)
+
+
+
+## Mixed Effect Model that use subjId as a random effect ####
+
+Clothing$sex = factor(Clothing$sex)
+Clothing$day = factor(Clothing$day)
+
+model0 <- lme(clo ~ tOut*tInOp*sex, random= ~ 1|subjId, data=Clothing, method="REML")
+logLik(model0)
+anova(model0)
+summary(model0)
+
+model1 <- lme(clo ~ tOut*tInOp+ tOut*sex+tInOp*sex+tOut*day+tInOp*day, random= ~ 1|subjId, data=Clothing, method="REML")
+logLik(model1)
+anova(model1)
+summary(model1)
+plot(model1)
+
+model2 <- lme(clo ~ tOut+tInOp+ tOut*sex+tInOp*sex+tOut*day+tInOp*day, random= ~ 1|subjId, data=Clothing, method="REML")
+logLik(model2)
+anova(model2)
+summary(model2)
+plot(model2)
+
+model3 <- lme(clo ~ tOut+ tOut*sex+sex+tOut*day+day, random= ~ 1|subjId, data=Clothing, method="REML")
+logLik(model3)
+anova(model3)
+summary(model3)
+plot(model3)
+# add weights as their is dispersion in the end of the residuals
+
+
+
+## Mixed Effect Model that 
+
+model3 <- lme(clo ~ tOut+ tOut*sex+sex+tOut*day+day, random= ~ 1|subjId, data=Clothing, method="REML")
+logLik(model3)
+anova(model3)
+summary(model3)
+plot(model3)
+
+
+
