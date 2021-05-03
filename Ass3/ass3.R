adb1bec16acc31e6c03ecbafbc20328fcb08d947
adamzenith <49098682+adamzenith@users.noreply.github.com>
2021-05-03 16:43:08 +0200

    make it

---
+++ Ass3/ass3.R
@@ -0,0 +1,27 @@
+clo = read.csv("clothingFullAss03.csv", header=TRUE, na.strings="?")
+
+summary(clo)
+
+#Plotting female stuff----
+par(mfrow=c(1,2))
+plot(clo$clo[clo$subjId==0 & clo$sex=="female"],ylim=c(0,1))
+for(i in 1:46){
+  points(clo$clo[clo$subjId==i & clo$sex=="female"],col=i+1)
+}
+
+#Plotting male stuff----
+plot(clo$clo[clo$subjId==4 & clo$sex=="male"],ylim=c(0,1))
+for(i in 1:46){
+  points(clo$clo[clo$subjId==i & clo$sex=="male"],col=i+1)
+}
+
+
+#Aggregate clo by subjId and Gender----
+means=aggregate(clo ~ subjId + sex, data=clo,FUN=mean)
+
+
+#linear mixed effect model----
+library(nlme)
+fit<-lme(clo~tOut+tInOp, random = ~1|sex, data=clo)
+summary(fit)
+plot(fit)
