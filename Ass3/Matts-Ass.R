bce741516e5e823ec5d901b625aba609dc2e6a49
adamzenith <49098682+adamzenith@users.noreply.github.com>
2021-05-06 13:39:43 +0200

    asd

--- Ass3/Matts-Ass.R
+++ Ass3/Matts-Ass.R
@@ -17,7 +17,7 @@ for(i in 1:46){
 
 
 #Aggregate clo by subjId and Gender----
-means=aggregate(clo ~ subjId + sex, data=clo,FUN=mean)
+means=aggregate(clo ~ subjId + s+, data=clo,FUN=mean)
 
 
 #linear mixed effect model----
@@ -29,5 +29,34 @@ plot(fit)
 
 
 #Part 2-------
+library(mvtnorm)
+
+
+
+
 library(lme4)
 fit0 <- lmer(clo~sex+(1|subjId),data=clo,REML=FALSE)
+logLik(fit0)
+X <- model.matrix(fit0)
+
+f <- function(beta, u) {
+  (beta[1] + rep(u[1:5], each = 7))/
+  (1 + exp((beta[2] - time)/beta[3])) }
+
+l <- function(u, beta, sigma, sigma.u) {
+  -sum(dnorm(x = circumference, mean = f(beta, u),
+             sd = sigma, log = TRUE)) -
+  sum(dnorm(x = u[1:5], sd = sigma.u, log = TRUE)) }
+
+l.LA <- function(theta) {
+  beta <- theta[1:3]
+  sigma <- exp(theta[4])
+  sigma.u <- exp(theta[5])
+  est <- nlminb(start = rep(0,5), objective = l, beta = beta,
+                sigma = sigma, sigma.u = sigma.u)
+  u <- est$par
+  l.u <- est$objective
+  Jac.f <- jacobian(func = f, x = u, beta = beta)
+  H <- crossprod(Jac.f)/sigma^2 + diag(1/sigma.u^2, 5)
+  l.u + 1/2 * log(det(H/(2 * pi))) }.
+
