dca64994cf7d571fb2347e7775db0447628cb48d
adamzenith <49098682+adamzenith@users.noreply.github.com>
2021-05-06 15:59:06 +0200

    as

--- Ass3/Matts-Ass.R
+++ Ass3/Matts-Ass.R
@@ -1,4 +1,4 @@
-clo = read.csv("clothingFullAss03.csv", header=TRUE, na.strings="?")
+clothing = read.csv("clothingFullAss03.csv", header=TRUE, na.strings="?")
 
 summary(clo)
 
@@ -30,33 +30,52 @@ plot(fit)
 
 #Part 2-------
 library(mvtnorm)
+no.persons <- max(clothing$subjId)
 
+fit0 <- lmer(clo~sex+(1|subjId),data=clothing,REML=FALSE)
+summary(fit0)
 
+nll <- function(theta,dat,X) { 
+  
+  mu <- X %*% t(t(theta[1:2]))
+  sigma <- exp(theta[3])
+  sigma.u <- exp(theta[4])
+  
+  L = 0 
+  
+  # loop over subjects
+  for(i in 0:no.persons){
+    
+    y_i <- dat$clo[dat$subjId==i & dat$da]
+    mu_i <- mu[dat$subjId==i]
+    n_i <- length(mu_i)
+    ones <- matrix(1,n_i,n_i)
+    V_i <- diag(n_i)*sigma+ones*sigma.u
+    
+    likelihood = log(1/((2*pi)^(n_i/2)*sqrt(det(V_i)))*exp(-0.5*t(y_i-mu_i)%*%solve(V_i)%*%(y_i-mu_i)))
+    L = L - likelihood 
+    
+  }
+  # output Likelihood  
+  L 
+}
 
-
-library(lme4)
-fit0 <- lmer(clo~sex+(1|subjId),data=clo,REML=FALSE)
-logLik(fit0)
 X <- model.matrix(fit0)
+theta0 <- c(0.5, -0.1, 0.1, 0.1)
+fit <- nlminb(theta0, nll, dat = clothing, X = X)
 
-f <- function(beta, u) {
-  (beta[1] + rep(u[1:5], each = 7))/
-  (1 + exp((beta[2] - time)/beta[3])) }
+fit
+sqrt(exp(fit$par[3:4]))
+fit0
+#####################
+### 1.2
 
-l <- function(u, beta, sigma, sigma.u) {
-  -sum(dnorm(x = circumference, mean = f(beta, u),
-             sd = sigma, log = TRUE)) -
-  sum(dnorm(x = u[1:5], sd = sigma.u, log = TRUE)) }
 
-l.LA <- function(theta) {
-  beta <- theta[1:3]
-  sigma <- exp(theta[4])
-  sigma.u <- exp(theta[5])
-  est <- nlminb(start = rep(0,5), objective = l, beta = beta,
-                sigma = sigma, sigma.u = sigma.u)
-  u <- est$par
-  l.u <- est$objective
-  Jac.f <- jacobian(func = f, x = u, beta = beta)
-  H <- crossprod(Jac.f)/sigma^2 + diag(1/sigma.u^2, 5)
-  l.u + 1/2 * log(det(H/(2 * pi))) }.
+## Test result 
+fit1 <- lmer(clo~sex+(1|subjId)+(1|subjId:day),data=dat,REML=FALSE)
+
+
+#####################
+### 1.3
+
 
